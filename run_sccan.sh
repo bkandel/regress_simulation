c8f26c260ce9e94fdd168d8f776e816de0a712b7
Ben Kandel <bkandel@seas.upenn.edu>
2013-07-11 17:04:35 -0400

    Modifying sparseness and placement of sources...

--- run_sccan.sh
+++ run_sccan.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ls img*.nii.gz > imgs.txt
+SPARSITY=0.025
+CLUSTER=4
 sccan --imageset-to-matrix [imgs.txt,mask.nii.gz] -o mymat.mha
-sccan --svd network[mymat.mha,mask.nii.gz,-0.2,demog.csv] -o out.nii.gz -n 4
+sccan --svd network[mymat.mha,mask.nii.gz,-${SPARSITY},demog.csv] -o out.nii.gz -n 4 --PClusterThresh $CLUSTER
