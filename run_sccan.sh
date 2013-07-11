5f0081192c2489ce5d9f7c05aee7cfdf6b0d362a
Ben Kandel <bkandel@seas.upenn.edu>
2013-07-11 18:24:22 -0400

    Non-negative sparsity--improves results.

--- run_sccan.sh
+++ run_sccan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ls img*.nii.gz > imgs.txt
 SPARSITY=0.025
-CLUSTER=4
+CLUSTER=8
 sccan --imageset-to-matrix [imgs.txt,mask.nii.gz] -o mymat.mha
-sccan --svd network[mymat.mha,mask.nii.gz,-${SPARSITY},demog.csv] -o out.nii.gz -n 4 --PClusterThresh $CLUSTER
+sccan --svd network[mymat.mha,mask.nii.gz,0.025,demog.csv] -o out.nii.gz -n 4 --PClusterThresh $CLUSTER
